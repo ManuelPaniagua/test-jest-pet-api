c08d70eea40aacd05c23f7e133bd869995c21676
Manuel <m.p.porroa@maxedv-beratung.de>
2024-06-14 12:19:25 +0200

    works

--- app.js
+++ app.js
@@ -1,22 +1,45 @@
-// app.js
+import express from 'express'
+import cors from 'cors'
+import swaggerUI from 'swagger-ui-express'
+import swaggerJSdoc from 'swagger-jsdoc'
 
-import express from 'express';
-import cors from 'cors';
-import petRoutes from './pets/routes/pets.routes.js';
+import petRoutes from './pets/routes/pets.routes.js'
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
+
+// swagger definition
+const swaggerSpec = {
+    definition: {
+        openapi: '3.0.0',
+        info: {
+            title: 'Pets API',
+            version: '1.0.0',
+        },
+        servers: [
+            {
+                url: `http://localhost:${port}`,
+            }
+        ]
+    },
+    apis: ['./pets/routes/*.js'],
+}
 
 /* Global middlewares */
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
+app.use(
+    '/api-docs',
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerJSdoc(swaggerSpec))
+)
 
 /* Routes */
-app.use('/pets', petRoutes);  // Monta las rutas de pets bajo /pets en la URL
+app.use('/pets', petRoutes)
 
 /* Server setup */
-app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(port, () => console.log(`⚡️[server]: Server is running at https://localhost:${port}`))
+}
 
-export default app;
+export default app
